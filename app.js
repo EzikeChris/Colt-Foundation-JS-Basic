0eb9b6e79115be3d3cb694cb1f0c57eaac2cf133
ezikechris <christophermbe1009@gmail.com>
2021-01-21 14:16:26 +0100

    booleanLogic

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ var lastName = prompt("what is your last name");
 var age = prompt("what is your age");
 var fullName =  firstName + "" + lastName;
 console.log("Your full Name is " + fullName);
+
+
+var age = prompt("what is your age");
+var days = age * 365;
+alert(age + "") 
