87539a36d6cfef258b2581d6c1b2049a4ffa593d
ezikechris <christophermbe1009@gmail.com>
2021-01-21 12:49:48 +0100

    html app.js files

---
+++ app.js
@@ -0,0 +1,5 @@
+var firstName = prompt("what is your first name");
+var lastName = prompt("what is your last name");
+var age = prompt("what is your age");
+var fullName =  firstName + "" + lastName;
+console.log("Your full Name is " + fullName);
